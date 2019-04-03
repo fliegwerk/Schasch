920d0254d44c8349219c2c39dd544faac2a5a938
alte <malte.arps@web.de>
2019-04-03 16:52:32 +0200

    removed method finding nailed pieces from pieces that are not capable of nailing others

--- src/Model/Chesspieces/Knight.cpp
+++ src/Model/Chesspieces/Knight.cpp
@@ -6,16 +6,20 @@
 
 
 //Knight can't nail Pieces
-std::optional<Tile *> Knight::updateNailedPieces(Tile *position) {
+std::optional<Tile*> Knight::updateNailedPieces(Tile* position) {
     return {};
 }
 
-std::vector<Tile> Knight::updatePossibleMoves(Tile *position, bool forKing) {
-    return std::vector<Tile>();
+std::vector<Tile*> Knight::updatePossibleMoves(Tile* position, bool forKing) {
+    std::vector<Tile*> possibleMoves;
+    //TODO add logic
+    return possibleMoves;
 }
 
-std::vector<Tile> Knight::updateCoveredPieces(Tile *position) {
-    return std::vector<Tile>();
+std::vector<Tile*> Knight::updateCoveredPieces(Tile* position) {
+    std::vector<Tile*> coveredPieces;
+    //TODO add logic
+    return coveredPieces;
 }
 
 std::string Knight::getName() {
